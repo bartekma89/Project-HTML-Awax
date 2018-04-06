71750964d99fa78c380ad382a11c6aaef8d7bf9d
bartekma89 <bartekma89@gmail.com>
2018-04-06 14:43:00 +0200

    added features to nav item - move to right section

--- src/js/main.js
+++ src/js/main.js
@@ -6,22 +6,40 @@
 
 		$('.nav-content').slideToggle();
 	});
-})(jQuery);
 
-/*(function() {
-	var navToggle = document.querySelector('#nav-toggle');
-	var navContent = document.querySelector('.nav-content');
-
-	navToggle.addEventListener('click', function(event) {
-		event.preventDefault();
-
-		this.classList.toggle('active');
-
-		if (navContent.style.display === 'block') {
-			navContent.style.display = 'none';
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 300) {
+			$('#backToTop').addClass('show');
 		} else {
-			navContent.style.display = 'block';
+			$('#backToTop').removeClass('show');
 		}
 	});
-})();
-*/
+
+	$('#backToTop').on('click', function(e) {
+		e.preventDefault();
+		$('html, body').animate({ scrollTop: 0 }, 2000);
+	});
+
+	/////
+
+	goTo('#nav-home', '#header', 1000);
+	goTo('#nav-about', '#about', 1100);
+	goTo('#nav-services', '#services', 1200);
+	goTo('#nav-projects', '#projects', 1300);
+	goTo('#nav-team', '#team', 1400);
+	goTo('#nav-clients', '#opinions', 1500);
+	goTo('#nav-price', '#price', 1600);
+	goTo('#nav-facts', '#facts', 1700);
+	goTo('#nav-contact', '#contact', 1800);
+
+	function goTo(elementClick, elementFind, time) {
+		$(elementClick).on('click', function(e) {
+			$('html').animate(
+				{
+					scrollTop: $(elementFind).offset().top,
+				},
+				time
+			);
+		});
+	}
+})(jQuery);
