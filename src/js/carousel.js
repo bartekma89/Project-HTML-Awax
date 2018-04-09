376527f01bd9d68d408280f0b39347d40ecae1c1
bartekma89 <bartekma89@gmail.com>
2018-04-09 14:40:08 +0200

    slider work

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,4 +1,4 @@
-$(function() {
+/*$(function() {
 	function forward() {
 		var currentImg = $('.active-slide');
 		var nextImg = currentImg.next();
@@ -21,4 +21,54 @@ $(function() {
 	}
 
 	setInterval(forward, 3000);
+});
+*/
+
+$(function() {
+	var $slider = $('#slider');
+	var $slides = $('#slider .slide-group');
+	var $slideWidth = $slides.width();
+
+	var seconds = 5;
+	var delay = 8;
+
+	var $controls = $('<div>').addClass('controls');
+	$slider.after($controls);
+
+	var slideClick = function() {
+		var $this = $(this);
+		$('.controls div').removeClass('current');
+		this.addClass('current');
+		var index = $this.index();
+		$slides.css('left', -1 * index * $slideWidth);
+	};
+
+	$('#slider .slide-group .slide').each(function(i) {
+		var $slide = $(this);
+		$slide.css('left', i * $slideWidth);
+		var $button = $('<div>');
+		$controls.append($button);
+		if (i === 0) {
+			$button.addClass('current');
+		}
+		$button.click(function() {
+			clearInterval(autoSlideInterval);
+			slideClick.apply(this);
+			setTimeout(function() {
+				setInterval(autoSlide, seconds * 1000);
+			}, delay * 1000);
+		});
+	});
+
+	var autoSlide = function() {
+		var $next = $('.controls .current').next();
+		if ($next.length) {
+			slideClick.apply($next);
+		} else {
+			var $first = $('.controls div').first();
+			slideClick.apply($first);
+		}
+	};
+
+	var autoSlideInterval = setInterval(autoSlide, seconds * 1000);
 });
