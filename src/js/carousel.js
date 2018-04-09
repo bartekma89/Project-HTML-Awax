275214b77e6156e464196a94bbdd8ffba3d5f921
bartekma89 <bartekma89@gmail.com>
2018-04-09 23:06:02 +0200

    added new slider

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,64 +1,20 @@
-/*$(function() {
-	function forward() {
-		var currentImg = $('.active-slide');
-		var nextImg = currentImg.next();
-
-		if (currentImg.length) {
-			currentImg.removeClass('active-slide');
-			nextImg.addClass('active-slide');
-			if (nextImg.length === 0) {
-				currentImg.removeClass('active-slide');
-				$('.slide')
-					.first()
-					.addClass('active-slide');
-			}
-		} else {
-			currentImg.removeClass('active-slide');
-			$('.slide')
-				.first()
-				.addClass('active-slide');
-		}
-	}
-
-	setInterval(forward, 3000);
-});
-*/
-
 $(function() {
 	var $slider = $('#slider');
 	var $slides = $('#slider .slide-group');
 	var $slideWidth = $slides.width();
+	var $controls = $('.controls');
+	var $button = $('.controls div');
 
 	var seconds = 5;
 	var delay = 8;
 
-	var $controls = $('<div>').addClass('controls');
-	$slider.after($controls);
-
 	var slideClick = function() {
 		var $this = $(this);
 		$('.controls div').removeClass('current');
-		this.addClass('current');
+		$this.addClass('current');
 		var index = $this.index();
 		$slides.css('left', -1 * index * $slideWidth);
 	};
-
-	$('#slider .slide-group .slide').each(function(i) {
-		var $slide = $(this);
-		$slide.css('left', i * $slideWidth);
-		var $button = $('<div>');
-		$controls.append($button);
-		if (i === 0) {
-			$button.addClass('current');
-		}
-		$button.click(function() {
-			clearInterval(autoSlideInterval);
-			slideClick.apply(this);
-			setTimeout(function() {
-				setInterval(autoSlide, seconds * 1000);
-			}, delay * 1000);
-		});
-	});
 
 	var autoSlide = function() {
 		var $next = $('.controls .current').next();
@@ -70,5 +26,19 @@ $(function() {
 		}
 	};
 
-	var autoSlideInterval = setInterval(autoSlide, seconds * 1000);
+	$('#slider .slide-group .slide').each(function(i) {
+		var $this = $(this);
+		$this.css('left', i * $slideWidth);
+		$button.click(function() {
+			clearInterval(autoSlide);
+			slideClick.apply(this);
+			setTimeout(start, delay * 1000);
+		});
+	});
+
+	function start() {
+		setInterval(autoSlide, seconds * 1000);
+	}
+
+	setTimeout(start, delay * 1000);
 });
